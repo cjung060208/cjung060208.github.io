118bb9986505d41df530a0dea8e8f2bc286a1b71
cjung060208 <jungchris0602@gmail.com>
2025-01-10 21:35:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ let slideIndex = 0;
 
 function showSlides() {
   const slides = document.querySelectorAll('.slide');
-  slides.forEach((slide) => (slide.style.display = 'none')); // Hide all slides
+  slides.forEach((slide) => (slide.style.display = 'none'));
 
   slideIndex++;
-  if (slideIndex > slides.length) slideIndex = 1; // Reset to first slide if at the end
+  if (slideIndex > slides.length) slideIndex = 1;
 
-  slides[slideIndex - 1].style.display = 'block'; // Show the current slide
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 2000); // Change image every x seconds
 }
 
-// Start the slideshow
 showSlides();
